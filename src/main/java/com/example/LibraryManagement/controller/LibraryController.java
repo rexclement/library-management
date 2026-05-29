3e8822c7bbb15cce20ac985f3f8c5b27b9061378
rexclement <rexclement2004@gmail.com>
2026-05-29 15:24:10 +0530

    pipeline testing 11

--- src/main/java/com/example/LibraryManagement/controller/LibraryController.java
+++ src/main/java/com/example/LibraryManagement/controller/LibraryController.java
@@ -20,7 +20,7 @@ public class LibraryController {
     @Autowired
     LibraryService libraryService;
 
-    // CREATE BOOK with validation
+    // CREATE BOOK
     @PostMapping("/addBook")
     public Response createBook(@RequestBody BookRequest book){
 
